79ea050c5e300d8ef9659e629f58be288fd51e9c
Dandaly Morales <dan.moralesv@gmail.com>
2022-05-22 11:29:48 -0400

    update debounce search by capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -14,11 +14,14 @@ export class PorCapitalComponent  {
   termino: string = ''
   hayError: boolean = false;
   capitales: Country[] = [];
+  mostrarSugerencias: boolean = false;
+  paisesSugeridos: Country[] = [];
 
   constructor(private paisServicio
     : PaisService) { }
 
   buscar(termino: string){
+    this.mostrarSugerencias = false;
     this.hayError = false
     this.termino = termino
 
@@ -30,6 +33,21 @@ export class PorCapitalComponent  {
       this.capitales = []
     });
   }
+
+  sugerencias(termino: string){
+    this.hayError = false;
+    this.termino = termino
+    this.mostrarSugerencias = true;
+    this.paisServicio.buscarCapital(termino).subscribe(paises => 
+      this.paisesSugeridos = paises.splice(0,5),
+      (err) => this.paisesSugeridos = []  
+    )
+  }
+
+  buscarSugerido(termino: string){
+    this.buscar(termino); 
+  }
+  
 
 
  
